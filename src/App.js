7f99613acbe20d223a25d03fc9ca6e344a88a2c8
victorcabeceira <victorfgcabeceira@gmail.com>
2019-04-22 15:54:57 -0300

    Sample routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import { Route, Switch, withRouter, Redirect, NavLink } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
+    let routes = (
+      <Switch>
+        <Route path='/planets' render={(props) => <div>Planets page</div>} />
+        <Route path='/' render={(props) => <div>Main page</div>} />
+        <Redirect to='/' />
+      </Switch>
+    );
     return (
-      <div className="App">
-        React SWAPI
+      <div>
+        <NavLink to='/'>Main</NavLink>
+        {' | '}
+        <NavLink to='/planets'>Planets</NavLink>
+
+        {routes}
+
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
