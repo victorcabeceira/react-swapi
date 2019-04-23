2e9c5af2a9d7f234a56cbb302414022801ff68f9
victorcabeceira <victorfgcabeceira@gmail.com>
2019-04-23 18:28:28 -0300

    Refactoring App.js to react hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { Route, withRouter, Redirect } from 'react-router-dom';
 import { AnimatedSwitch } from 'react-router-transition';
@@ -9,28 +9,26 @@ import CustomLayout from './hoc/Layout/Layout';
 
 import './App.css';
 
-class App extends Component {
-  render() {
-    let routes = (
-      <AnimatedSwitch
-        atEnter={{ opacity: 0 }}
-        atLeave={{ opacity: 0 }}
-        atActive={{ opacity: 1 }}
-        className="switch-wrapper"
-      >
-        <Route path='/planets' render={(props) => <Planets {...props} />} />
-        <Route path='/' render={(props) => <MainPage {...props} />} />
-        <Redirect to='/' />
-      </AnimatedSwitch>
-    );
-    return (
-      <div>
-        <CustomLayout {...this.props}>
-          {routes}
-        </CustomLayout>
-      </div>
-    );
-  }
+const app = props => {
+  let routes = (
+    <AnimatedSwitch
+      atEnter={{ opacity: 0 }}
+      atLeave={{ opacity: 0 }}
+      atActive={{ opacity: 1 }}
+      className="switch-wrapper"
+    >
+      <Route path='/planets' render={(props) => <Planets {...props} />} />
+      <Route path='/' render={(props) => <MainPage {...props} />} />
+      <Redirect to='/' />
+    </AnimatedSwitch>
+  );
+  return (
+    <div>
+      <CustomLayout {...props}>
+        {routes}
+      </CustomLayout>
+    </div>
+  );
 }
 
-export default withRouter(App);
+export default withRouter(app);
