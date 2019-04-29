0249b3a8e7d9536c218c1b78de784cb05cbd151f
victorcabeceira <victorfgcabeceira@gmail.com>
2019-04-29 15:07:44 -0300

    Re-styling planets

--- src/containers/planets/Planets.js
+++ src/containers/planets/Planets.js
@@ -1,38 +1,52 @@
 import React, { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { Row, Col } from 'react-flexbox-grid';
 
 import Loader from '../../components/UI/Loader/Loader';
 import * as actions from '../../store/actions/index';
+import classes from './Planets.module.css';
 
 const planets = props => {
   useEffect(() => {
     props.onFetchPlanets(0);
   }, []);
 
-  let planets = <div><Loader style={{ background: '#FFD700' }}/></div>
+  let planets = <div><Loader style={{ background: '#FFD700' }} /></div>
 
   if (!props.loading) {
     planets = (
-      <div>
-        <p>Planets count: {props.planets.count}</p>
-        {props.planets.results.map(planet => {
-          const idRegex = /(\b\d*\b)(?!\1)/g;
-          const planetUrlId = idRegex.exec(planet.url);
+      <Row style={{ margin: 0 }} middle='xs' center='xs'>
+        <Col xs>
+          <Row middle='xs' center='xs' className='mv-md'>
+            <Col xs>
+              <div className={classes.Planets}>Planets count : {props.planets.count}</div>
+            </Col>
+          </Row>
 
-          return (
-            <NavLink
-              to={`/planets/${planetUrlId[0]}`}
-              exact
-              key={planetUrlId[0]}
-            >
-              <div>
-                Name: {planet.name}
-              </div>
-            </NavLink>
-          )
-        })}
-      </div>
+          <Row middle='xs' center='xs' className='mv-md'>
+            {props.planets.results.map(planet => {
+              const idRegex = /(\b\d*\b)(?!\1)/g;
+              const planetUrlId = idRegex.exec(planet.url);
+
+              return (
+                <Col xs={6}>
+                  <NavLink
+                    to={`/planets/${planetUrlId[0]}`}
+                    exact
+                    key={planetUrlId[0]}
+                  >
+                    <div>
+                      Name: {planet.name}
+                    </div>
+                  </NavLink>
+                </Col>
+              )
+            })}
+          </Row>
+
+        </Col>
+      </Row>
     )
   }
 
