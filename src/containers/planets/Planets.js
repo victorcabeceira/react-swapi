56ae91727e23203d4cb7aa5c24f1a6a9cf7c5b6b
victorcabeceira <victorfgcabeceira@gmail.com>
2019-04-30 11:29:50 -0300

    Using images randomly as background-img of Card

--- src/containers/planets/Planets.js
+++ src/containers/planets/Planets.js
@@ -11,10 +11,23 @@ import Loader from '../../components/UI/Loader/Loader';
 import * as actions from '../../store/actions/index';
 import classes from './Planets.module.css';
 
+import sun from '../../assets/images/planets/sun.png';
+import venus from '../../assets/images/planets/venus.png';
+import mercury from '../../assets/images/planets/mercury.png';
+import earth from '../../assets/images/planets/earth.png';
+import moon from '../../assets/images/planets/moon.png';
+import mars from '../../assets/images/planets/mars.png';
+import jupiter from '../../assets/images/planets/jupiter.png';
+import saturn from '../../assets/images/planets/saturn.png';
+import neptune from '../../assets/images/planets/neptune.png';
+import uranus from '../../assets/images/planets/uranus.png';
+
 const planets = props => {
   useEffect(() => {
     props.onFetchPlanets(0);
   }, []);
+
+  const planetsImgArray = [sun, venus, mercury, earth, moon, mars, jupiter, saturn, neptune, uranus];
 
   let planets = <div><Loader style={{ background: '#FFD700' }} /></div>
 
@@ -33,9 +46,10 @@ const planets = props => {
               const idRegex = /(\b\d*\b)(?!\1)/g;
               const planetUrlId = idRegex.exec(planet.url);
 
-              const wantedProperties = ['name', 'climate', 'gravity', 'terrain', 'population', 'residents', 'films' ];
+              const wantedProperties = ['name', 'climate', 'gravity', 'terrain', 'population', 'residents', 'films'];
 
               const filteredPlanet = Object.entries(planet).filter(p => wantedProperties.includes(p[0]));
+              const randomPlanetImg = Math.floor(Math.random() * 9)
 
               return (
                 <Col xs={5} className="m-md" key={planetUrlId[0]}>
@@ -44,9 +58,12 @@ const planets = props => {
                     key={planetUrlId[0]}
                     style={{ textDecoration: 'none', color: '#E8E8E8' }}
                   >
-                    <Card style={{ background: '#5A5A5A' }}>
+                    <Card style={{
+                      backgroundImage: `url(${planetsImgArray[randomPlanetImg]}), linear-gradient(rgba(0, 0, 0, 0.8), rgba(255, 255, 255, 0.8))`,
+                      backgroundSize: 'cover',
+                    }}>
                       <CardActionArea>
-                        <CardContent >
+                        <CardContent>
                           <div className={classes.CardContentTitle}>
                             Planet {planet.name}
                           </div>
