bda9ebf01f65a34bf860028d61df9c5ed1ce9f63
victorcabeceira <victorfgcabeceira@gmail.com>
2019-04-30 12:21:06 -0300

    Displaying planet information

--- src/containers/planets/planet/Planet.js
+++ src/containers/planets/planet/Planet.js
@@ -1,15 +1,26 @@
 import React, { useEffect } from 'react';
-// import { Link } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { Row, Col } from 'react-flexbox-grid';
 
-// import Card from '@material-ui/core/Card';
-// import CardActionArea from '@material-ui/core/CardActionArea';
-// import CardContent from '@material-ui/core/CardContent';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
 
 import Loader from '../../../components/UI/Loader/Loader';
 import * as actions from '../../../store/actions/index';
 import classes from './Planet.module.css';
+
+import sun from '../../../assets/images/planets/sun.png';
+import venus from '../../../assets/images/planets/venus.png';
+import mercury from '../../../assets/images/planets/mercury.png';
+import earth from '../../../assets/images/planets/earth.png';
+import moon from '../../../assets/images/planets/moon.png';
+import mars from '../../../assets/images/planets/mars.png';
+import jupiter from '../../../assets/images/planets/jupiter.png';
+import saturn from '../../../assets/images/planets/saturn.png';
+import neptune from '../../../assets/images/planets/neptune.png';
+import uranus from '../../../assets/images/planets/uranus.png';
 
 const planet = props => {
 
@@ -19,25 +30,62 @@ const planet = props => {
     props.onFetchPlanet(planetUrlId[0]);
   }, []);
 
+  const filteredPlanet = Object.entries(props.planet).filter(p => !['randomImgNumber'].includes(p[0]));
+  const planetsImgArray = [sun, venus, mercury, earth, moon, mars, jupiter, saturn, neptune, uranus];
+
   let planet = <div><Loader style={{ background: '#FFD700' }} /></div>
+
+  const randomRgbaGenerator = () => {
+    const g = Math.floor(Math.random() * 80);
+    const b = Math.floor(Math.random() * 80);
+    const r = Math.floor(Math.random() * 80);
+    return 'rgba(' + r + ', ' + g + ', ' + b + ', 1)';
+  }
 
   if (!props.loading) {
     planet = (
       <Row style={{ margin: 0 }} middle='xs' center='xs'>
         <Col xs>
-          <Row middle='xs' center='xs' className='mv-md'>
-            <Col xs>
-              <div className={classes.Planet}>Planet  {props.planet.name}</div>
-            </Col>
-          </Row>
+          <Card style={{
+            margin: '16px 8px',
+            backgroundImage: `
+              url(${planetsImgArray[props.planet.randomImgNumber]}),
+              linear-gradient(
+                ${randomRgbaGenerator()},
+                rgba(30, 30, 30, 0.6),
+                rgba(30, 30, 30, 0.4)`,
+            backgroundSize: 'contain',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center'
+          }}>
+            <CardActionArea>
+              <CardContent>
+                <div className={classes.CardContentTitle}>
+                  Planet {planet.name}
+                </div>
+
+                {filteredPlanet.map(fp => {
+                  let property = fp[0].charAt(0).toUpperCase() + fp[0].slice(1)
+                  property = property.replace('_', ' ')
+                  let value = Array.isArray(fp[1]) ? fp[1].length : fp[1];
+                  return (
+                    <div key={`${fp.name}_${fp[0]}`} className={classes.CardContent}>
+                      <div className={classes.ContentProperty}>{property} : </div>
+                      <div className={classes.ContentData}>{value}</div>
+                    </div>
+                  )
+                })}
+              </CardContent>
+            </CardActionArea>
+          </Card>
         </Col>
-      </Row>
+      </Row >
     )
   }
 
   return (
     <div>
-      { planet }
+      {planet}
     </div>
   )
 }
