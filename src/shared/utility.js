d31c4fd9a41392053a136fd0c97252042b15d4ff
victorcabeceira <victorfgcabeceira@gmail.com>
2019-05-02 12:51:26 -0300

    Creating filterUrl function

--- src/shared/utility.js
+++ src/shared/utility.js
@@ -4,3 +4,11 @@ export const updateObject = (oldObject, updatedProperties) => {
     ...updatedProperties
   }
 };
+
+export const filterUrl = (fullUrl) => {
+  const apiCollectionRegex = /([a-z]+)(?=\/\d*\/)/g;
+  const idRegex = /(\b\d*\b)(?!\1)/g;
+  const urlCollection = (apiCollectionRegex.exec(fullUrl))[0];
+  const urlId = (idRegex.exec(fullUrl))[0];
+  return `/${urlCollection}/${urlId}/`
+}
