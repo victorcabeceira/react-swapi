0db5b3de72d74526f99957e9dae7007d07b9740f
victorcabeceira <victorfgcabeceira@gmail.com>
2019-05-02 13:23:22 -0300

    Using Button to display NavLink children

--- src/components/Navigation/CustomNavLink/CustomNavLink.js
+++ src/components/Navigation/CustomNavLink/CustomNavLink.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 
 const customNavLink = props => (
   <NavLink
     to={props.customTo}
     key={props.customKey}
-    style={{ textDecoration: 'none', color: '#E8E8E8', ...props.style }}
+    style={{ textDecoration: 'none', color: '#E8E8E8' }}
   >
-    {' '}{props.divContent}
+    <Button variant='outlined' color='inherit' style={{ ...props.style }}>
+      {props.divContent}
+    </Button>
   </NavLink>
 )
 
