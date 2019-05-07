c195933f7e516fb705ea29349a5d460f27082998
victorcabeceira <victorfgcabeceira@gmail.com>
2019-05-07 15:19:00 -0300

    Adding People images

---
+++ src/assets/images/people/index.js
@@ -0,0 +1,25 @@
+import people1 from './1.png';
+import people2 from './2.png';
+import people3 from './3.png';
+import people4 from './4.png';
+import people5 from './5.png';
+import people6 from './6.png';
+import people7 from './7.png';
+import people8 from './8.png';
+import people9 from './9.png';
+import people10 from './10.png';
+import people11 from './11.png';
+
+export default [
+  people1,
+  people2,
+  people3,
+  people4,
+  people5,
+  people6,
+  people7,
+  people8,
+  people9,
+  people10,
+  people11,
+];
