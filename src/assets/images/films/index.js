a4463a8eda237ffbd38f06b2f0230c9db13c17fb
victorcabeceira <victorfgcabeceira@gmail.com>
2019-05-07 17:46:55 -0300

    Adding films images

---
+++ src/assets/images/films/index.js
@@ -0,0 +1,17 @@
+import film1 from './1.jpg';
+import film2 from './2.jpg';
+import film3 from './3.jpg';
+import film4 from './4.jpg';
+import film5 from './5.jpg';
+import film6 from './6.jpg';
+import film7 from './7.jpg';
+
+export default [
+  film1,
+  film2,
+  film3,
+  film4,
+  film5,
+  film6,
+  film7,
+];
