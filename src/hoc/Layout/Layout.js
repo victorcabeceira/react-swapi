a4df72dcbccd2a644283e2912fc908504b9b4531
victorcabeceira <victorfgcabeceira@gmail.com>
2019-05-09 16:52:00 -0300

    Improving general Layout

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -2,20 +2,37 @@ import React from 'react';
 
 import CustomHeader from '../../components/Navigation/Header/Header';
 import CustomFooter from '../../components/Navigation/Footer/Footer';
+import Card from '@material-ui/core/Card';
+
+import bgImage from '../../assets/images/bg.jpg';
 
 import classes from './Layout.module.css';
 
 const layout = props => {
+  // Photo by Lubo Minar on Unsplash
+  const layoutBackGround = {
+    backgroundImage: `url(${bgImage})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    width: '100%',
+    height: '100%',
+  }
+
   return (
-    <div className={classes.Layout}>
-      <CustomHeader {...props} />
+    <div style={layoutBackGround}>
+      <div className={classes.Layout}>
+        <CustomHeader {...props} />
 
-      <div className={classes.Content}>
-        {props.children}
-      </div>
+        <div className={classes.MainContent}>
+          <Card style={{ background: '#363636' }} className={classes.Content}>
+            {props.children}
+          </Card>
+        </div>
 
-      <div className={classes.Footer}>
-        <CustomFooter {...props} />
+        <div className={classes.Footer}>
+          <CustomFooter {...props} />
+        </div>
       </div>
     </div>
   )
