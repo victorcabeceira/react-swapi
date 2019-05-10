a31f3689f36465715cdc577fceae0a89efe7c36a
victorcabeceira <victorfgcabeceira@gmail.com>
2019-05-10 13:50:01 -0300

    Adding about me section of aboutPage

--- src/components/Navigation/AboutPage/AboutPage.js
+++ src/components/Navigation/AboutPage/AboutPage.js
@@ -9,6 +9,17 @@ const aboutPage = props => {
   const link = (url, name) => (
     <a className={classes.Link} href={url} alt={name}>{name}</a>
   )
+
+  const imageStyle = {
+    backgroundImage: `url('https://avatars3.githubusercontent.com/u/5403782?s=460&v=4')`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundSize: 'contain',
+    width: '100%',
+    height: '100%',
+    opacity: 0.8
+  }
+
   return (
     <Grid fluid className={classes.Grid}>
       <Row middle='xs' className={classes.MainRow}>
@@ -24,6 +35,7 @@ const aboutPage = props => {
           <Typography align='justify' gutterBottom variant='body1' className={classes.Content}>
             It consumes data from the {link('https://swapi.co/', 'SWAPI')}, an API that has all data from the 7 canon movies,
             displayed in a programatically-formatted set of data, where the data available is as stated in the statistics.
+            I designed this website, but the main focus was to display the set of skills related to the technologies used.
           </Typography>
         </Col>
 
@@ -85,12 +97,38 @@ const aboutPage = props => {
 
       <hr className={classes.Hr} />
 
-      <Row>
-        <Col xs={12} md={4}>
-          Describe myself
+      <Row className={classes.AboutMeRow}>
+        <Col xs={12} md={8}>
+          <Row center='xs' middle='xs'>
+            <Col xs={12}>
+              <Typography align='center' gutterBottom variant='body1' className={classes.AboutMeTitle}>
+                About me
+              </Typography>
+            </Col>
+          </Row>
+
+          <Row center='xs' middle='xs'>
+            <Col xs>
+              <Typography align='justify' gutterBottom variant='body1' className={classes.DisclaimerContent}>
+                Hi! My name is <strong className={classes.Strong}>Victor</strong> and I'm a frontend developer.
+              </Typography>
+              <Typography align='justify' gutterBottom variant='body1' className={classes.DisclaimerContent}>
+                I love building things and discussing ideas, also, I love video games.
+                I live in Brasília, Brazil and my dream is to move to Canada one day.
+              </Typography>
+              <Typography align='justify' gutterBottom variant='body1' className={classes.DisclaimerContent}>
+                I have worked with <strong className={classes.Strong}>Service Management</strong>, {' '}
+                <strong className={classes.Strong}>Data Administration</strong> and with <strong className={classes.Strong}>Development</strong>.
+                My main experience is with development, specifically with Javascript and React.
+              </Typography>
+              <Typography align='justify' gutterBottom variant='body1' className={classes.DisclaimerContent}>
+                Like what you see? Get in touch through one of the options below on the footer!
+              </Typography>
+            </Col>
+          </Row>
         </Col>
-        <Col xs={12} md={4}>
-          Image with opacity
+        <Col xs={12} md={4} >
+          <div style={imageStyle}></div>
         </Col>
       </Row>
 
